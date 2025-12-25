27eb3502dd834c148fca671fa7941efd10c2253c
Julien Nicolas <jjnicolas@gmail.com>
2025-12-24 16:56:39 -0800

    Add wikipediaUrl field to TaxonCommonName model
    - Add wikipediaUrl property to cache Wikipedia URLs from iNaturalist API
    - Update initializer to accept optional wikipediaUrl parameter
    - Maintains backward compatibility with default nil value

--- Sources/ObserveData/TaxonCommonName.swift
+++ Sources/ObserveData/TaxonCommonName.swift
@@ -7,13 +7,15 @@ public final class TaxonCommonName {
     public var taxonId: Int = 0  // CloudKit requires default value
     public var locale: String = "en"  // "en", "fr", "es" - CloudKit requires default value
     public var commonName: String?
+    public var wikipediaUrl: String?  // Wikipedia URL from iNaturalist API
     public var cachedAt: Date = Date()  // CloudKit requires default value - for 1-week cache expiry
 
-    public init(taxonId: Int, locale: String, commonName: String?) {
+    public init(taxonId: Int, locale: String, commonName: String?, wikipediaUrl: String? = nil) {
         self.id = "\(taxonId)_\(locale)"
         self.taxonId = taxonId
         self.locale = locale
         self.commonName = commonName
+        self.wikipediaUrl = wikipediaUrl
         self.cachedAt = Date()
     }
 
